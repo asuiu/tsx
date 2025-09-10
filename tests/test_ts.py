0f1088796f40286b182304dfe89b2e34dc5c9256
ASU <andrei.suiu@gmail.com>
2025-09-10 18:39:20 +0300

    fix test_isoweekday test failing on ubuntu 3.9

--- tests/test_ts.py
+++ tests/test_ts.py
@@ -348,8 +348,11 @@ class TestTS(TestCase):
         ts = TS.from_iso("2022-12-07T00:00:01", utc=True)
         self.assertEqual(ts.isoweekday(), 3)
         ts = TS.from_iso("2022-12-07T00:00:00+02", utc=False)
-        self.assertEqual(ts.isoweekday(utc=False), 3)
-        self.assertEqual(ts.isoweekday(), 2)
+        # compute expected values from the same ISO instant so test is independent of machine local tz
+        expected_local = datetime.fromisoformat("2022-12-07T00:00:00+02:00").isoweekday()
+        expected_utc = datetime.fromisoformat("2022-12-07T00:00:00+02:00").astimezone(timezone.utc).isoweekday()
+        self.assertEqual(ts.isoweekday(utc=False), expected_local)
+        self.assertEqual(ts.isoweekday(), expected_utc)
 
     def test_default_utc(self):
         expected = TS.from_iso("2022-12-07T00:00:00Z")
